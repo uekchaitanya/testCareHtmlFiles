bc417126ab9f052faf79a1993e299134fb9b3172
uekchaitanya <uekchaitanya@gmail.com>
2024-09-19 04:02:47 -0400

    Update customScript.js
    js added print to all fields

--- customScript.js
+++ customScript.js
@@ -1,6 +1,16 @@
 console.log('Test JS Line 1 ');
 
 document.addEventListener('DOMContentLoaded', function() {
+    // Log all input fields on the page
+    const inputFields = document.querySelectorAll('input, select, textarea');
+    inputFields.forEach((field) => {
+        console.log('Field Name: ' + field.name + ', Field Value: ' + field.value);
+    });
+});
+
+console.log('Test JS Line 11 ');
+
+document.addEventListener('DOMContentLoaded', function() {
     function getQueryParam(param) {
         const urlParams = new URLSearchParams(window.location.search);
         const value = urlParams.get(param);
